883df42d2bc174f753004cb53df1e89840e84e89
20161104567 <846129391@qq.com>
2018-09-26 11:30:00 +0800

    计算器
    计算器的显示

---
+++ ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  Li
+//
+//  Created by 20161104567 on 2018/9/26.
+//  Copyright © 2018年 20161104567. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var Show: UIView!
+    @IBAction func One1(_ sender: Any) {
+    }
+    @IBAction func Two2(_ sender: Any) {
+    }
+    @IBAction func Three3(_ sender: Any) {
+    }
+    @IBAction func Four4(_ sender: Any) {
+    }
+    @IBAction func Five5(_ sender: Any) {
+    }
+    @IBAction func Six6(_ sender: Any) {
+    }
+    @IBAction func Seven7(_ sender: Any) {
+    }
+    @IBAction func Eight8(_ sender: Any) {
+    }
+    @IBAction func Nine9(_ sender: Any) {
+    }
+    @IBAction func Zieo0(_ sender: Any) {
+    }
+    @IBAction func Add(_ sender: Any) {
+    }
+    @IBAction func Reduction(_ sender: Any) {
+    }
+    @IBAction func Take(_ sender: Any) {
+    }
+    @IBAction func Addition(_ sender: Any) {
+    }
+    @IBAction func Equal(_ sender: Any) {
+    }
+    @IBAction func Delete(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
